37f41948ce72ebd1a15664d8a32be12019c1af1f
Alejandro Nava <alejandro.nava@rappi.com>
2019-10-24 19:07:25 +0000

    Merged in feature/PF-1269-configure-bitbucket-pipelines (pull request #5)
    PF-1269 - Configure bitbucket pipelines
    
    * Configure bitbucket pipelines
    
    * Remove javafx dependency
    
    Approved-by: Ivan Vercinsky <ivan.vercinsky@rappi.com>

--- rules-interpreter/src/main/kotlin/com/rappi/fraud/rules/parser/conditions/NumericCondition.kt
+++ rules-interpreter/src/main/kotlin/com/rappi/fraud/rules/parser/conditions/NumericCondition.kt
@@ -3,13 +3,12 @@ package com.rappi.fraud.rules.parser.conditions
 import com.rappi.fraud.analang.ANALexer
 import com.rappi.fraud.analang.ANAParser.NumberContext
 import com.rappi.fraud.rules.parser.evaluators.FieldEvaluator
-import javafx.util.converter.BigIntegerStringConverter
 
 class NumericCondition : Condition<NumberContext> {
     override fun eval(ctx: NumberContext, data: Map<String, *>): Boolean {
         val ruleCtx = FieldEvaluator(data).visitFieldRecursive(ctx.field())
                 ?: throw RuntimeException("Invalid Operation ${ctx.field().text}")
-        val ctxValue = BigIntegerStringConverter().fromString(ruleCtx.toString()).toLong()
+        val ctxValue = ruleCtx.toString().toLong()
         val literalValue = ctx.value.text.toLong()
         return when (ctx.op.type) {
             ANALexer.LT -> ctxValue < literalValue
